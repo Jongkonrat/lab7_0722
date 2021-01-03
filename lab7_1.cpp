0098fe9889ed9572a8af7a24f36884ac1cec639e
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-01-03 23:42:54 +0700

    completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -2,16 +2,33 @@
 using namespace std;
 
 int main()
-{
+{ 
+string text_S = "You have received Super Ultra Rare Unit!!!\n";
+string text_A = "You have received 5 gems.\n";
+string text_B = "You have received 1 gems.\n";
+string text_C = "You have received 2000 coins.\n";
+string text_D = "You have received very KAK items.\n";
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
+  if(rank == 'S'){
+    cout << text_S << text_A << text_B << text_C << text_D;
   }
+  else if(rank == 'A'){
+    cout << text_A << text_B << text_C << text_D;
+  }
+  else if(rank == 'B'){
+    cout << text_B << text_C << text_D;
+  }
+  else if(rank == 'C'){
+    cout << text_C << text_D;
+  }
+  else if(rank == 'D'){
+    cout << text_D;
+  }
+  else{
+    cout << "";
+  }
+
   return 0;
 }
